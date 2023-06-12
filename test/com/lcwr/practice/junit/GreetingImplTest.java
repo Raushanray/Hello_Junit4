9387b4049f54c6c376af821462107c5beb00832d
Raushanray <raushankuray1998@gmail.com>
2023-06-12 18:30:34 +0530

    add @Before and @After annotation and then check

--- test/com/lcwr/practice/junit/GreetingImplTest.java
+++ test/com/lcwr/practice/junit/GreetingImplTest.java
@@ -2,14 +2,23 @@ package com.lcwr.practice.junit;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 	
+	private Greeting greeting;
+	@Before
+	public void Setup() {
+		System.out.println("Setup");
+		greeting = new GreetingImpl();
+		
+	}
+	
 	@Test
 	public void greetShouldReturnAValidOutput() {
 		System.out.println("greetShouldReturnAValidOutput");
-		 Greeting greeting = new GreetingImpl();
 		String result = greeting.greet("Junit");
 		assertNotNull(result);
 		assertEquals("Hello Junit", result);
@@ -17,14 +26,18 @@ public class GreetingImplTest {
 	
 	@Test(expected  = IllegalArgumentException.class)
 	public void greetShouldThrowAnException_For_NameIsNull() {
-		Greeting greeting = new GreetingImpl();
 		System.out.println("greetShouldThrowAnException_For_NameIsNull");
 		greeting.greet(null);
 	}
 	@Test(expected  = IllegalArgumentException.class)
 	public void greetShouldThrowAnException_For_NameIsBlank() {
-		Greeting greeting = new GreetingImpl();
 		System.out.println("greetShouldThrowAnException_For_NameIsBlank");
 		greeting.greet("");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown");
+		greeting = null;
 	}
 }
